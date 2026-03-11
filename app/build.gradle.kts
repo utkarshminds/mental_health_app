b4eff0713fe7ed14b38856cce57751c9d4afb86c
utkarshminds <pranav.nerurkar@utkarshminds.com>
2026-03-11 08:57:03 +0530

    first

---
+++ app/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+}
+
+android {
+    namespace = "com.example.utkarsh"
+
+    defaultConfig {
+        applicationId = "com.example.utkarsh"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+}
